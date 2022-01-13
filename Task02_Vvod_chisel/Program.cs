f60292a29fe52d8d7c624cdac0b9028a9f486cd7
Inna <shvartz_i@mail.ru>
2022-01-13 05:33:26 +0300

    Task 2 added

--- Task02_Vvod_chisel/Program.cs
+++ Task02_Vvod_chisel/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Определить сколько чисел больше 0 введено с клавиатуры
+
+Console.WriteLine("Вводите чиcла или CTRL+Z to exit:");
+string line;
+int count = 0;
+
+while ((line = Console.ReadLine()) != null)
+{
+    int i = int.Parse(line);
+    if (i > 0) count++;
+}
+Console.WriteLine($"{count} чисел больше 0");
+
+// do
+// {
+//     line = Console.ReadLine();
+//     if (line != null)
+//     {
+//         int i = int.Parse(line);
+//         if (i > 0) count++;
+//     }
+// } while (line != null);
+//
+// Console.WriteLine($"{count} чисел больше 0");
