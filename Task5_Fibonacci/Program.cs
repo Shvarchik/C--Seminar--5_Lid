ea34aee107b6468947f8331aed523be8aef13864
Inna <shvartz_i@mail.ru>
2022-01-17 06:50:06 +0300

    Task5_Fibonacci added

---
+++ Task5_Fibonacci/Program.cs
@@ -0,0 +1,19 @@
+﻿// 45 Показать числа Фибоначчи
+
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.WriteLine($"f ({i}) = {array[i]}");
+}
+Console.WriteLine ("Введите число для последовательности Фибоначчи:");
+int N = int.Parse(Console.ReadLine() ?? "0");
+double [] f = new double [N+1];
+f[0] = 0;
+f[1] = 1;
+for (int i=2;i < f.Length;i++)
+    f[i] = f[i-1] + f [i-2];
+
+PrintArray(f);
+
+    
+
