a8972f1bd61115721de1128c78e37c4b136b4321
Inna <shvartz_i@mail.ru>
2022-02-04 06:57:55 +0300

    Оптимизированная задача ScalingFromString

--- Tsak6_ScalingFromString/Program.cs
+++ Tsak6_ScalingFromString/Program.cs
@@ -6,7 +6,6 @@
 // при k = 2 получаем "(0,0) (4,0) (4,4) (0,4)"
 
 using System.Linq;
-
 // Подсчет вершин фигуры по числу запятых в строке
 int NumberOfPoints(string str)
 {
@@ -16,58 +15,42 @@ int NumberOfPoints(string str)
             count++;
     return count;
 }
-// из исходной строки создать массив строк "число,число"
-string[] CoordinatesString(string str, int n)
+// из исходной строки создать массив чисел
+int[] ParseCoordinates(string str, int n)
 {
-    string[] coordinates = new string[n];
+    int[] coordinates = new int[n*2];
     int j = 0;
+    string point = String.Empty;
     for (int i = 0; i < str.Length; i++)
     {
-        string point = String.Empty;
-        while (i < str.Length && str[i] != ' ')
+        while (i < str.Length && (str[i]!='(' && str[i]!=','))
         {
-            if (str[i] == '(' || str[i] == ')')
-            {
-                i++;
-                continue;
-            }
-            point += str[i];
             i++;
         }
-        coordinates[j] = point;
-        j++;
-
+        i++;
+        if (i < str.Length)
+        {
+            while (str[i]!=',' && str[i] !=')')
+            {
+                point += str[i];
+                i++;
+            }            
+            coordinates[j] = int.Parse(point);
+            j++;
+            point = String.Empty;
+            i--;           
+        }
     }
     return coordinates;
 }
-// из строки "число,число" получить массив из двух чисел
-int[] ParseCoordinates(string str)
-{
-    int[] point = new int[2];
-    int j = 0;
-    string tempString = String.Empty;
-    for (int i = 0; i <= str.Length; i++)
-    {
-        while (i < str.Length && str[i] != ',')
-        {
-            tempString += str[i];
-            i++;
-        }
-        point[j] = int.Parse(tempString);
-        j++;
-        tempString = String.Empty;
-    }
-    return point;
-}
+
 Console.WriteLine("Введите координаты вершин:");
 string spisok = Console.ReadLine() ?? "0";
 Console.WriteLine("Введите коэффициент масштабирования:");
 int k = int.Parse(Console.ReadLine() ?? "0");
 int amountOfPoints = NumberOfPoints(spisok);
-string[] coordinateStringArray = CoordinatesString(spisok, amountOfPoints);
+int[] coordinateArray = ParseCoordinates(spisok, amountOfPoints);
 Console.WriteLine("Новые координаты:");
-for (int i = 0; i < amountOfPoints; i++)
-{
-    int[] coordinateArray = ParseCoordinates(coordinateStringArray[i]);
-    Console.Write($"({coordinateArray[0] * k},{coordinateArray[1] * k}) ");
-}
+for (int i = 0; i < amountOfPoints*2; i+=2)
+     Console.Write($"({coordinateArray[i] * k},{coordinateArray[i+1] * k}) ");
+
